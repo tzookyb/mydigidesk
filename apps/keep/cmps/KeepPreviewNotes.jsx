c3d255150f97715e259501e19701bd9a91c727f7
tzookyb <tzookyb@gmail.com>
2020-08-26 21:21:37 +0300

    preview controls, style,  and actions

--- apps/keep/cmps/KeepPreviewNotes.jsx
+++ apps/keep/cmps/KeepPreviewNotes.jsx
@@ -1,11 +1,21 @@
-export function KeepPreviewNotes({ notes }) {
+export function KeepPreviewNotes({ currFilter, notes, noteTrash, noteBgc, notePin, noteMail }) {
 
     return notes.map((note) => {
+        const pin = note.isPinned ? 'pinned' : 'pin';
+        const remove = note.isTrash ? 'remove' : 'trash';
+        const trashView = (currFilter === 'trash') ? true : false;
         return (
             <div key={note.id} className="note-card">
+                <div className="note-pin" onClick={() => notePin(note)}>{pin}</div>
                 <div className="note-title">{note.content.title}</div>
                 <div className="note-content">{note.content.text}</div>
-            </div>
+                <div className="note-controls">
+                    <div className="note-btn note-trash" onClick={() => noteTrash(note)}>{remove}</div>
+                    {trashView && <div className="note-btn note-untrash" onClick={() => noteTrash(note, true)}>unTrash</div>}
+                    <div className="note-btn note-bgc" onClick={() => noteBgc(note.id)}>Bgc</div>
+                    <div className="note-btn note-mail" onClick={() => noteMail(note.id)}>Email</div>
+                </div>
+            </div >
         )
     })
 }
--- apps/keep/pages/Keep.jsx
+++ apps/keep/pages/Keep.jsx
@@ -13,11 +13,49 @@ export class Keep extends React.Component {
     componentDidMount() {
         const filterBy = this.props.match.params.filter;
         console.log("Keep -> componentDidMount -> filterBy", filterBy)
+
         this.setState({ filterBy });
         keepService.getNotes(filterBy)
             .then((notes) => {
                 this.setState({ notes })
             })
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        // console.log('props', prevProps, 'state', prevState);
+    }
+
+    refresh() {
+        keepService.getNotes(this.state.filterBy)
+            .then((notes) => {
+                this.setState({ notes })
+            })
+    }
+
+    noteTrash = (note, unTrash = false) => {
+        if (note.isTrash && !unTrash) {
+            keepService.removeNote(note.id)
+                .then(this.refresh());
+        } else {
+            const status = note.isTrash ? false : true;
+            keepService.updateNote(note.id, 'isTrash', status)
+                .then(this.refresh());
+        }
+    }
+
+    noteBgc = (noteId) => {
+        console.log("Keep -> noteBgc -> noteId", noteId)
+
+    }
+    notePin = (note) => {
+        const status = note.isPinned ? false : true;
+        keepService.updateNote(note.id, 'isPinned', status)
+            .then(this.refresh());
+    }
+
+    noteMail = (noteId) => {
+        console.log("Keep -> noteMail -> noteId", noteId)
+
     }
 
 
@@ -29,7 +67,7 @@ export class Keep extends React.Component {
                 <KeepSideBar />
                 <div className="keep-main-area">
                     <KeepAddNote />
-                    <KeepPreviewNotes notes={this.state.notes} />
+                    <KeepPreviewNotes currFilter={this.state.filterBy} notes={notes} noteTrash={this.noteTrash} noteBgc={this.noteBgc} notePin={this.notePin} noteMail={this.noteMail} />
                 </div>
             </section>
         )
