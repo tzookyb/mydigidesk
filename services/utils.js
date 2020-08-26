d9d8cafa5bd7ca6eff02cd7bf7a5fc2ce564c5a7
xXDaniel123 <daniel91dante@gmail.com>
2020-08-26 12:38:14 +0300

    added utils

---
+++ services/utils.js
@@ -0,0 +1,17 @@
+// returns random numbers
+function getRandomInteger(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+// random color
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
