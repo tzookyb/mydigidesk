423b6c52bfe53f38c5ee18e2a8b163892a49188a
tzookyb <tzookyb@gmail.com>
2020-08-26 12:41:56 +0300

    make id

--- services/utils.js
+++ services/utils.js
@@ -15,3 +15,12 @@ function getRandomColor() {
     }
     return color;
 }
+
+function makeId(length = 5) {
+    var txt = '';
+    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for (var i = 0; i < length; i++) {
+        txt += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return txt;
+}
