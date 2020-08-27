7a93afce381205f2996e12e041acf94d286e17f2
tzookyb <tzookyb@gmail.com>
2020-08-27 20:59:43 +0300

    .

--- apps/keep/cmps/note/LabelEditor.jsx
+++ apps/keep/cmps/note/LabelEditor.jsx
@@ -13,9 +13,10 @@ export class LabelEditor extends React.Component {
         this.setState(prevState => ({ isShown: !prevState.isShown }));
     }
 
-    saveLabel = (key) => {
-        if (key !== 'Enter') return;
-
+    saveLabel = (ev) => {
+        if (ev.key !== 'Enter') return;
+        this.props.onNoteLabel(this.props.note, this.state.input)
+        this.setState({ isShown: false, input: '' })
     }
 
     changeInput = (value) => {
@@ -27,7 +28,7 @@ export class LabelEditor extends React.Component {
             <React.Fragment>
                 <div className="note-btn note-label" onClick={this.onToggleLabelEditor}>Label</div>
                 {(this.state.isShown) && <div className="label-editor">
-                    <input className="label-input" value={this.state.input} placeholder="Enter new label..." onKeyPress={(ev) => { this.saveLabel(ev.key) }} onChange={(ev) => this.changeInput(ev.target.value)} type="text" />
+                    <input className="label-input" value={this.state.input} placeholder="Enter new label" onKeyPress={(ev) => { this.saveLabel(ev) }} onChange={(ev) => this.changeInput(ev.target.value)} type="text" />
                     <GlobalLabels note={this.props.note} onNoteLabel={this.props.onNoteLabel} />
                 </div>}
             </React.Fragment>
--- apps/keep/cmps/note/Pin.jsx
+++ apps/keep/cmps/note/Pin.jsx
@@ -2,7 +2,5 @@ export function Pin({ onNotePin, note }) {
 
     const pin = note.isPinned ? 'unpin' : 'pin';
 
-    const output = <div className="note-pin" onClick={() => onNotePin(note)}>{pin}</div>;
-
-    return output;
+    return <div className="note-pin" onClick={() => onNotePin(note)}>{pin}</div>;
 }
