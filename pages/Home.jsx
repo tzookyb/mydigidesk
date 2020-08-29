184e6fd9d43a6e78a9eb6dc87b1da26669fbdea5
tzookyb <tzookyb@gmail.com>
2020-08-29 08:18:37 +0300

    added bg image

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -2,8 +2,8 @@ export class Home extends React.Component {
 
     render() {
         return (
-            <section>
-                <h2>Welcome to MyDigiDesk</h2>
+            <section className="home-container">
+                <span className="home-title">Welcome to MyDigiDesk</span>
             </section>
         )
     }
